d75d0639d9291d2cfc22c5da0c333a1b0b565351
EIEIZA555 <s6503051634039@email.kmutnb.ac.th>
2025-09-26 21:24:13 +0700

    Upd 2
    Upd 2

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,7 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import ragai from '../../assets/ragaifront.png';
+import flutter from '../../assets/flutter.png';
+import webboard from '../../assets/webboard.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,19 +10,19 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={ragai}
           link="https://github.com/EIEIZA555/proj-rag-v1"
           h3="Project RAG-AI(Frontend)"
           p="RAG App"
         />
         <ProjectCard
-          src={freshBurger}
+          src={flutter}
           link="https://github.com/EIEIZA555/Wongnai_Cloning"
           h3="Wongnai Cloning"
           p="Wongnai app"
         />
         <ProjectCard
-          src={hipsster}
+          src={webboard}
           link="https://github.com/EIEIZA555/work_webboard"
           h3="Webboard"
           p="Webboard webapp"
